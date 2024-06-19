b91910fef9fe24cfdff76d761cce1d3344ad1577
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-20 00:19:03 +0530

    renamed MindsDBTool to AIMindTool

--- libs/community/langchain_community/tools/__init__.py
+++ libs/community/langchain_community/tools/__init__.py
@@ -28,6 +28,9 @@ if TYPE_CHECKING:
         tool,
     )
 
+    from langchain_community.tools.mindsdb import (
+        AIMindTool
+    )
     from langchain_community.tools.ainetwork.app import (
         AINAppOps,
     )
@@ -320,6 +323,7 @@ if TYPE_CHECKING:
     )
 
 __all__ = [
+    "AIMindTool",
     "AINAppOps",
     "AINOwnerOps",
     "AINRuleOps",
